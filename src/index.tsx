0d7dfe04f2b6a5cfaf17af5733883317fda45ee2
Menas Erian <meena5.erian@gmail.com>
2022-10-14 16:09:13 +0400

    init

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const dafault_export = {type: "default_export"};
+
+const ExportedComponent = () => <h1>Some heading</h1>
+
+export default dafault_export;
+
+export {ExportedComponent};
